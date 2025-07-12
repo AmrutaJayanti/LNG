57c80ca5a02a93b3e070e3066d87e06eb6e941db
Amruta Jayanti <142327526+AmrutaJayanti@users.noreply.github.com>
2025-07-12 09:41:55 +0530

    Update isAuth.js

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -1,18 +1,17 @@
 import jwt from "jsonwebtoken";
 
 const isAuth = (req, res, next) => {
-  try {
-    const token = req.cookies.token;
-    if (!token) {
-      return res.status(401).json({ message: "Token missing" });
-    }
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized: No token" });
+  }
 
+  try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.userId;
+    req.userEmail = decoded.email;
     next();
   } catch (error) {
-    console.log(error)
-        return res.status(500).json({message:`isauth error ${error}`})
+    return res.status(401).json({ message: "Unauthorized: Invalid token" });
   }
 };
 
