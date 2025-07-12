737d5cd073efc2eecd1f525f3f906bedcd87243f
Amruta Jayanti <142327526+AmrutaJayanti@users.noreply.github.com>
2025-07-12 09:42:52 +0530

    Update user.controllers.js

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,18 +1,18 @@
 import uploadOnCloudinary from "../config/cloudinary.js"
 import User from "../models/user.model.js"
 
-export const getCurrentUser=async (req,res)=>{
-try {
-    let user=await User.findById(req.userId).select("-password")
-    if(!user){
-        return res.status(400).json({message:"user not found"})
+export const getCurrentUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.userEmail }).select("-password");
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
     }
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: `current user error ${error}` });
+  }
+};
 
-    return res.status(200).json(user)
-} catch (error) {
-    return res.status(500).json({message:`current user error ${error}`})
-}
-}
 
 
 export const editProfile=async (req,res)=>{
@@ -37,16 +37,15 @@ export const editProfile=async (req,res)=>{
     }
 }
 
-export const getOtherUsers=async (req,res)=>{
-    try {
-        let users=await User.find({
-            _id:{$ne:req.userId}
-        }).select("-password")
-        return res.status(200).json(users)
-    } catch (error) {
-        return res.status(500).json({message:`get other users error ${error}`})
-    }
-}
+export const getOtherUsers = async (req, res) => {
+  try {
+    const users = await User.find({ email: { $ne: req.userEmail } }).select("-password");
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).json({ message: `get other users error ${error}` });
+  }
+};
+
 
 export const search =async (req,res)=>{
     try {
